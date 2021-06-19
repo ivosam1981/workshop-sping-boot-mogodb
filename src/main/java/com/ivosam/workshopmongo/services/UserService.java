14f03aa3dafe78673b4048133c8ea1fb58e74fad
Ivo G Guerreiro <ivgest@gmail.com>
2021-06-19 03:37:13 -0300

    obtendo usuario por ID

--- src/main/java/com/ivosam/workshopmongo/services/UserService.java
+++ src/main/java/com/ivosam/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ivosam.workshopmongo.domain.User;
 import com.ivosam.workshopmongo.repositories.UserRepository;
+import com.ivosam.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +18,14 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 		
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto Não Encontrado");
+		}
+		return user;
 	}
 
 }
