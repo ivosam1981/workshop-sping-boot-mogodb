7c1ea8f81a62ee4cb3ad011b7c11a4364dd9a529
Ivo G Guerreiro <ivgest@gmail.com>
2021-06-22 16:36:06 -0300

    consulta simples com queryMethods

--- src/main/java/com/ivosam/workshopmongo/services/PostService.java
+++ src/main/java/com/ivosam/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.ivosam.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Objeto Não Encontrado");
 		}
 		return post;
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
